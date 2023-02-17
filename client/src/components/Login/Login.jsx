6b54209e6079fe8506dbd869a19e8398b9bf00c7
Alistair Saldanha <shaanalistair47@gmail.com>
2023-02-17 16:01:06 +0530

    Added Login Auth

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,22 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Login.css';
-
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+
+    const navigate = useNavigate();
+    const [userLogin, setUserLogin] = useState({
+        email: "",
+        password: "",
+    })
+    const handleLoginSubmit = (e) => {
+        e.preventDefault();
+        if (!userLogin.email || !userLogin.password) {
+            alert("Invalid Inputs");
+            return;
+        }
+        axios.post("http://localhost:3001/login", { userLogin }).then((data) => {
+            navigate("/homepage", { state: data.data.userDeets });
+        }).catch(err => {
+            console.log(err);
+            navigate("/error");
+        })
+    }
+    // Handling Inputs
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setUserLogin({
+            ...userLogin,
+            [name]: value
+        })
+    }
+
     return (
         <>
             <main className='login-page'>
-                <form action="/login" className='login-form' method='POST'>
+                <form className='login-form'>
+                    <h1>LOGIN</h1>
                     <div className='form-input'>
-                        <label name="userName">Username</label>
-                        <input type="text" />
+                        <label name="email">Email</label>
+                        <input type="email" name="email" value={userLogin.email} onChange={handleInputChange} placeholder="Enter your email" required />
                     </div>
                     <div className='form-input'>
                         <label name="password">Password</label>
-                        <input type="password" />
+                        <input type="password" name="password" value={userLogin.password} onChange={handleInputChange} placeholder="Enter your password" required />
                     </div>
-                    <div>
-                        <input type="submit" value={"SUBMIT"} className='submit-btn' />
+                    <div className='form-btn'>
+                        <button type='submit' className='submit-btn' onClick={(e) => {
+                            handleLoginSubmit(e);
+                        }}>SUBMIT</button>
+                        <p>OR</p>
+                        <button type='submit' className='submit-btn' onClick={(e) => {
+                            navigate("/signup");
+                        }}>SIGNUP</button>
                     </div>
                 </form>
             </main>
