abe3159fafa420912a632ab1bdedda415744622c
Alistair Saldanha <shaanalistair47@gmail.com>
2023-04-03 18:04:28 +0530

    Integrated saavn homepage

--- client/src/components/MainPage/Home/Home.jsx
+++ client/src/components/MainPage/Home/Home.jsx
@@ -1,15 +1,11 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-// import apiClient from '../../../spotify';
 
 import './Home.css';
 function Home({ setCurrTrack }) {
-    const [newReleases, setNewReleases] = useState(null);
-    const [trendingToday, SetTrendingToday] = useState(null);
-    const [likedSongs, setLikedSongs] = useState(null);
-
-    const [allSongs, setAllSongs] = useState(null);
+    const [charts, setCharts] = useState([]);
     useEffect(() => {
+    // SPOTIFY CODE
         // apiClient.get("browse/new-releases?limit=5").then((res) => {
         //     setNewReleases(res.data.albums.items);
         // })
@@ -19,30 +15,83 @@ function Home({ setCurrTrack }) {
         // apiClient.get('me/tracks?limit=5').then(res => {
         //     setLikedSongs(res.data.items.map(item => { return item.track }));
         // })
-        axios.get("https://saavn.me/modules?language=hindi,english").then(res => {
-            let topTrendingPlaylistId = res.data.data.charts[0].id;
-            axios.get(`https://saavn.me/playlists?id=${topTrendingPlaylistId}`).then(res => {
-                SetTrendingToday(res.data.data.songs.slice(0, 5));
+
+    // SAAVN CODE
+        axios.get("https://saavn.me/modules?language=hindi,english")
+            .then(res => {
+                // var top10Charts = res.data.data.charts.slice(0, 10);
+                return res.data.data.charts.slice(0, 5);
             })
-        })
-        axios.get('http://localhost:3001/csvdata')
-            .then((response) => {
-                setAllSongs(response.data.slice(100, 105)); // the CSV data in JSON format
+            .then(res => {
+                res.forEach(ele => {
+                    const chartId = ele.id;
+                    const chartTitle = ele.title;
+                    axios.get(`https://saavn.me/playlists?id=${chartId}`).then(response => {
+                        console.log(response.data);
+                        setCharts(prevState => [...prevState, { title: chartTitle, songs: response.data.data.songs.slice(0, 5) }])
+                    })
+                })
             })
-            .catch((error) => {
-                console.log(error);
-            });
+        // axios.get('http://localhost:3001/csvdata')
+        //     .then((response) => {
+        //         setAllSongs(response.data.slice(100, 105)); // the CSV data in JSON format
+        //     })
+        //     .catch((error) => {
+        //         console.log(error);
+        //     });
     }, [])
     const handlePlayTrack = (currSong) => {
         // axios.get(`https://saavn.me/search/albums?query=${currSong.name}`).then(res => console.log(res.data));
         setCurrTrack(currSong);
     }
-
     return (
         <div className="main-container">
             <div className="home-content">
                 <h2>Home</h2>
-                <h2 className='home-title'>All Songs</h2>
+                {/* OPTIMISED */}
+                {charts.map(chart => {
+                    return (<>
+                        <h2 className='home-title'>{chart.title}</h2>
+                        <div className='homecards'>
+                            {chart.songs?.map((song) => {
+                                return (
+                                    <div
+                                        className="homecard"
+                                        onClick={() => {
+                                            handlePlayTrack(song);
+                                        }}
+                                    >
+                                        <img
+                                            src={song?.image[2]?.link}
+                                            className="homecard-image"
+                                            alt="Playlist-Art"
+                                        />
+                                        <p className="homecard-title">{song.name}</p>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </>)
+                })}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                {/* OLD CODE */}
+                {/* <h2 className='home-title'>All Songs</h2>
                 <div className='homecards'>
                     {allSongs?.map((song) => (
                         <div
@@ -60,24 +109,9 @@ function Home({ setCurrTrack }) {
                             <p className="homecard-title">{song.track_name}</p>
                         </div>
                     ))}
-                </div>
-                <h2 className='home-title'>Trending Today</h2>
-                <div className='homecards'>
-                    {trendingToday?.map((track) => (
-                        <div
-                            className="homecard"
-                            key={track.id}
-                        >
-                            <img
-                                src={track.image[2].link}
-                                className="homecard-image"
-                                alt="Playlist-Art"
-                            />
-                            <p className="homecard-title">{track.name}</p>
-                        </div>
-                    ))}
-                </div>
-                <h2 className='home-title'>Liked Songs</h2>
+                </div> */}
+                {/* <Carousel /> */}
+                {/* <h2 className='home-title'>Liked Songs</h2>
                 <div className='homecards'>
                     {likedSongs?.map((likedSong) => (
                         <div
@@ -93,8 +127,8 @@ function Home({ setCurrTrack }) {
                             <p className="homecard-title">{likedSong.name}</p>
                         </div>
                     ))}
-                </div>
-                <h2 className='home-title'>Top Releases</h2>
+                </div> */}
+                {/* <h2 className='home-title'>Top Releases</h2>
                 <div className='homecards'>
                     {newReleases?.map((newRelease) => (
                         <div
@@ -110,7 +144,7 @@ function Home({ setCurrTrack }) {
                             <p className="homecard-title">{newRelease.name}</p>
                         </div>
                     ))}
-                </div>
+                </div> */}
             </div>
         </div>
     )
